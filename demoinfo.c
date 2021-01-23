aa880f9287504482ea8e2ac56bf8cfdb711fbcc0
Mads Mogensen <mail@madsmogensen.dk>
2021-01-23 20:50:37 +0100

    topdown

--- demoinfo.c
+++ demoinfo.c
@@ -27,45 +27,71 @@ struct demo_header
 };
 
 
-void die(const char *const msg)
+struct demo_header read_demo_header(FILE *const file);
+void print_demo_header(const struct demo_header demo_header);
+void free_demo_header(struct demo_header demo_header);
+
+char *read_string(FILE *const file, const size_t count);
+int read_int(FILE *const file);
+float read_float(FILE *const file);
+
+void die(const char *const msg);
+
+
+int main(int argc, char *argv[])
 {
-  fprintf(stderr, "%s\n", msg);
-
-  exit(EXIT_FAILURE);
-}
-
-char *read_string(FILE *const file, const size_t count)
-{
-  char *const str = malloc(sizeof(str[0]) * count);
-
-  if (fread(str, sizeof(str[0]), count, file) != count) {
-    die("Could not read string");
+  if (argc != 2) {
+    fprintf(stderr, "USAGE: %s <file>\n", argv[0]);
+    return EXIT_FAILURE;
   }
 
-  if (str[count - 1] != 0) { die("No null terminator"); }
+  FILE *const file = fopen(argv[1], "r");
 
+  if (!file) {
+    perror("Could not open file");
+    return EXIT_FAILURE;
+  }
 
-  return str;
+  struct demo_header demo_header;
+
+  read_demo_header(file);
+
+  fclose(file);
+
+  print_demo_header(demo_header);
+
+  free_demo_header(demo_header);
+
+  return EXIT_SUCCESS;
 }
 
-int read_int(FILE *const file)
+
+struct demo_header read_demo_header(FILE *const file)
 {
-  int i;
-
-  if (fread(&i, sizeof(i), 1, file) != 1) { die("Could not read int"); }
+  struct demo_header demo_header;
 
 
-  return i;
-}
+  demo_header.header = read_string(file, HEADER_LEN);
 
-float read_float(FILE *const file)
-{
-  float f;
+  if (strcmp(demo_header.header, expected_header) != 0) {
+    die("Header was not what was expected");
+  }
 
-  if (fread(&f, sizeof(f), 1, file) != 1) { die("Could not read float"); }
+  demo_header.demo_protocol = read_int(file);
+  demo_header.network_protocol = read_int(file);
+
+  demo_header.server_name = read_string(file, STRING_LEN);
+  demo_header.client_name = read_string(file, STRING_LEN);
+  demo_header.map_name = read_string(file, STRING_LEN);
+  demo_header.game_directory = read_string(file, STRING_LEN);
+
+  demo_header.playback_time = read_float(file);
+  demo_header.ticks = read_int(file);
+  demo_header.frames = read_int(file);
+  demo_header.sign_on_length = read_int(file);
 
 
-  return f;
+  return demo_header;
 }
 
 void print_demo_header(const struct demo_header demo_header)
@@ -105,46 +131,45 @@ void free_demo_header(struct demo_header demo_header)
   free(demo_header.game_directory);
 }
 
-int main(int argc, char *argv[])
+
+char *read_string(FILE *const file, const size_t count)
 {
-  if (argc != 2) {
-    fprintf(stderr, "USAGE: %s <file>\n", argv[0]);
-    return EXIT_FAILURE;
+  char *const str = malloc(sizeof(str[0]) * count);
+
+  if (fread(str, sizeof(str[0]), count, file) != count) {
+    die("Could not read string");
   }
 
-  FILE *const file = fopen(argv[1], "r");
+  if (str[count - 1] != 0) { die("No null terminator"); }
 
-  if (!file) {
-    perror("Could not open file");
-    return EXIT_FAILURE;
-  }
 
-  struct demo_header demo_header;
+  return str;
+}
 
-  demo_header.header = read_string(file, HEADER_LEN);
+int read_int(FILE *const file)
+{
+  int i;
 
-  if (strcmp(demo_header.header, expected_header) != 0) {
-    die("Header was not what was expected");
-  }
+  if (fread(&i, sizeof(i), 1, file) != 1) { die("Could not read int"); }
 
-  demo_header.demo_protocol = read_int(file);
-  demo_header.network_protocol = read_int(file);
 
-  demo_header.server_name = read_string(file, STRING_LEN);
-  demo_header.client_name = read_string(file, STRING_LEN);
-  demo_header.map_name = read_string(file, STRING_LEN);
-  demo_header.game_directory = read_string(file, STRING_LEN);
+  return i;
+}
 
-  demo_header.playback_time = read_float(file);
-  demo_header.ticks = read_int(file);
-  demo_header.frames = read_int(file);
-  demo_header.sign_on_length = read_int(file);
+float read_float(FILE *const file)
+{
+  float f;
 
-  fclose(file);
+  if (fread(&f, sizeof(f), 1, file) != 1) { die("Could not read float"); }
 
-  print_demo_header(demo_header);
 
-  free_demo_header(demo_header);
+  return f;
+}
 
-  return EXIT_SUCCESS;
+
+void die(const char *const msg)
+{
+  fprintf(stderr, "%s\n", msg);
+
+  exit(EXIT_FAILURE);
 }
